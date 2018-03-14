affd98a15463040d26a39f3a5d31fe11066cd850
Andrew Scott <a.clayton.scott@gmail.com>
2018-03-14 13:01:18 -0500

    Updated MariaDB installation check to consider brew installed instances

--- BurstXBundle/Resources/is_maria_db_installed.sh
+++ BurstXBundle/Resources/is_maria_db_installed.sh
@@ -3,8 +3,23 @@
 #
 # Created by Andrew Scott on 2/5/18.
 
+
+
 if [ ! -d "/usr/local/mariadb" ]; then
-    echo "0"
+    which -s brew
+    if [[ $? != 0 ]] ; then
+        # MariaDB isn't in the default location and brew isn't installed
+        echo "0"
+    else
+        if brew ls --versions mariadb > /dev/null; then
+        # MariaDB isn't in the default location, brew is installed, and brew returns a version of MariaDB is installed
+            echo "1"
+        else
+            # MariaDB isn't in the default location, brew is installed, and there is no known version of MariaDB
+            echo "0"
+        fi
+    fi
 else
+    # MariaDB exists in the default location
     echo "1"
 fi
