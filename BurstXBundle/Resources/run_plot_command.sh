4392a0d9c51e6c759632fc7497f98c146362cf2d
Andrew Scott <a.clayton.scott@gmail.com>
2018-03-14 23:32:56 -0500

    Spent 2 hours rewriting the plotting launcher because AppleScript CANT HANDLE A SPACE IN THE PATH

--- BurstXBundle/Resources/run_plot_command.sh
+++ BurstXBundle/Resources/run_plot_command.sh
@@ -4,7 +4,10 @@
 # Created by Andrew Scott on 2/10/18.
 
 cd ~/Library/Application\ Support/BurstXBundle/
-DIR=$(pwd)
-osascript -e "do shell script \"$DIR/tmp_plot_command.sh\" with administrator privileges" > log.this.too
-LS_FILES=$(ls -la)
-echo "$DIR"
+# Applescript is utter shit
+osascript -e 'set short_name to do shell script "whoami"' \
+          -e 'set p to "/Users/" & short_name & "/Library/Application Support/BurstXBundle/tmp_plot_command.sh"' \
+          -e 'set filePath to POSIX file p' \
+          -e 'set contentsOfFile to (read file filePath)' \
+          -e 'do shell script contentsOfFile with administrator privileges'
+
